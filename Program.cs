799114e688a15c68ec6298fa2bd8607e29699316
kian chachi <chachi@kians-MacBook-Pro.local>
2019-08-20 19:38:55 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Declare a variable called rings that is of the Int Type
+                // int numRings = 2;
+                // if (numRings >= 5)
+                // {
+                //     Console.WriteLine("You are welcome to join the party");
+                // }
+                // else
+                // {
+                //     Console.WriteLine("Go win some more rings");
+                // }
+
+
+
+
+                // int numRings = 3;
+                // if (numRings >= 5)
+                // {
+                //     Console.WriteLine("You are welcome to join the party");
+                // }
+                // else if (numRings > 2)
+                // {
+                //     Console.WriteLine($"Decent...but {numRings} rings aren't enough");
+                // }
+                // else
+                // {
+                //     Console.WriteLine("Go win some more rings");
+                // }
+
+
+
+                // int numRings = 5;
+                // string name = "Kobe";
+                // if (numRings >= 5 && name == "Kobe")
+                // {
+                // Console.WriteLine($"Welcome to the party {name}, congratulations on your {numRings} championships!");
+                // }
+
+
+
+                int numRings = 5;
+                int numOfAllStarAppearances = 17;
+                if (numRings >= 5 || numOfAllStarAppearances > 3)
+                {
+                Console.WriteLine("Welcome, you are truly a legend");
+                }
+
+
+
+
+
+
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
